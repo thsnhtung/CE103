ff28e634aa576756ba13ea1f4aea7d1bd92a168a
thsnhtung <19522491@gm.uit.edu.vn>
2021-06-04 18:22:29 +0700

    Parse message

--- Main/main.c
+++ Main/main.c
@@ -1,4 +1,4 @@
-#include <REGX52.H>
+#include <REGX51.H>
 //Khai bao ket noi IC 74595
 
 sbit IDX0_IC595_SHCP = P0^0;	  //xung clock
@@ -17,16 +17,20 @@ sbit IDX3_IC595_SHCP = P1^3;	  //xung clock
 sbit IDX3_IC595_DS = P1^4;	  // chan data
 sbit IDX3_IC595_STCP = P1^5;	// chan chot du lieu 
 
+sbit ledStart = P3^4;
+sbit ledUart = P3^5;
 sbit button = P3^6;
 sbit led1 = P3^7;
+
 unsigned char STATE_595_0 = 0x00;
 unsigned char STATE_595_1 = 0x00;
 unsigned char STATE_595_2 = 0x00;
 unsigned char STATE_595_3 = 0x00;
 ////////////////////////////////
-unsigned char n;
-unsigned char START_BYTE=1;
-unsigned char MODE=1;
+unsigned char r_data;
+unsigned char START_BYTE= 0;
+unsigned char MODE= 0;
+unsigned char Count = 0 ;
 
 
 
@@ -117,87 +121,97 @@ void OUT_BIT_LED(unsigned char idx, unsigned char idx_595)
 	}
 }
  /////////////////////////////
+
+
 //khoi tao
 void UART_Init()
 {
 	TMOD = 0x20;		/* Timer 1, 8-bit auto reload mode */
-	TH1 = 0xFD;		/* Load value for 9600 baud rate */
+	TH1 = 0xFD;		/* Load value for 9600 baud rate */ //F3
 	SCON = 0x50;		/* Mode 1, reception enable */
 	TR1 = 1;
 	IE=0X90;		/* Start timer 1 */
 }
 
-/////////////////////////////
- void check()
-{
-  if(START_BYTE!=0 && MODE!=0)
-  {
-   OUT_BIT_LED(START_BYTE,MODE);
-  }
 
-  
-
-}
-//ngat nhan UART
-//unsigned char temp=0;
-unsigned char dem=0;
-void ISR_UART(void) interrupt 4
+void Mode1_Handler(unsigned char Data)
 {
- 
- if(RI == 1)           //Kiem tra xem la ngat nhan hay truyen
- {	
-    n = SBUF;   //nhan du lieu
-    RI = 0;
-	if(n==0)
+	if (Data >= 0 && Data < 256)
 	{
-	  if (START_BYTE==0)
-	  {
-	  START_BYTE=1;
-	  }
-	  else if (START_BYTE=1)
-	  { 
-	  	 if(n<4)
-		 {  MODE=n;  }
-		 else { START_BYTE=0;MODE=0;}
-	  
-	  
-	   }
-	
+		OUT_BYTE_LED(Data, Count);
+		Count++;
+		if (Count == 4)
+		{
+			Count = 0 ;
+			START_BYTE = 0 ;
+			MODE = 0 ;
+		}
 	}
 	else
 	{
-	 if(START_BYTE==0){OUT_BIT_LED(1,1);}
-	 else {
-	 if(MODE==0 && n<4){MODE=n;}
-	 else if(MODE==0 && n>=4){OUT_BIT_LED(1,1);}
-	 
-	 if(MODE!=0)
-	 { if(dem<=4)
-	  { OUT_BIT_LED(n,MODE);
-	     dem++;
-    	}
-		else 
-		{
-		 OUT_BIT_LED(1,1);
-		 dem=0;
-		}
-	 
-	 }
-	 
-	 
-	   }
-
-	
-	
-	
+		// Out of range data 307,2
+		// led on
 	}
- }
-
- else if (TI==1)
- {
- 	TI=0;
- }
 }
+
+
+
+void Receive_Handler(unsigned char Data)
+{
+	TI = 0 ; 
+	SBUF = Data ; 
+	Data -= 0x30;
+	while(TI ==0);
+	TI = 0;
+	if (START_BYTE == 0 && Data == 0)			// chua co du lieu va nhan duoc start_byte
+	{
+		ledStart = 0 ;
+		START_BYTE = 1;
+	}
+	else if (START_BYTE == 1 && MODE == 0 && (Data > 0 && Data < 3))  // neu da start va mode chua co, thi check xem mode byte co dung format k __ mode 1 mode 2
+	{
+		MODE = Data;
+	}
+	else if (START_BYTE !=0 && MODE != 0)
+	{
+		if (MODE == 1)
+		{
+			Mode1_Handler(Data);
+		}
+		else if (MODE == 2)
+			Mode1_Handler(Data);
+		else
+		{
+			// Turn on LED
+			// should not reach here
+		}
+	}
+	else				// chua hop sai
+	{
+		START_BYTE = 0 ;
+		MODE = 0 ;
+	}
+	RI = 0;
+}
+
+
+void ISR_UART(void) interrupt 4
+{
+	if(RI == 1)           //Kiem tra xem la ngat nhan hay truyen
+	{	
+		ledUart = ~ledUart;
+		r_data = SBUF;   				//nhan du lieu
+		Receive_Handler(r_data);
+	}
+
+	else if (TI==1)
+	{
+		TI=0;
+	}
+}
+
+
+
 
 void main()
 {
@@ -207,7 +221,13 @@ void main()
 	OUT_BYTE_LED(0x00,1);
 	OUT_BYTE_LED(0x00,2);
 	OUT_BYTE_LED(0x00,3);
-   UART_Init();
+	
+	OUT_BYTE_LED(0x00,0);
+	OUT_BYTE_LED(0x00,1);
+	OUT_BYTE_LED(0x00,2);
+	OUT_BYTE_LED(0x00,3);
+  
+	UART_Init();
    	 
 	while(1)
 	{
